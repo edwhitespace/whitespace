729b0f8db70a3d826625e36ed1e02230b77d1521
Edward Manson <ed@white.space>
2019-03-07 11:44:47 +0000

    Content updates, back to top button implemented and new partner logos added.

--- js/scripts.js
+++ js/scripts.js
@@ -122,3 +122,20 @@ $(function(){
     $('.circle-button').toggleClass('circle-button2')    
   }
 })
+
+// Back to Top Button
+
+var btn = $('#back-to-top-button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
